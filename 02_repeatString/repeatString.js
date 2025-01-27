0e0814a1cc218936f3a83da6886d376b87279886
mcher0 <mansuperman.ms@gmail.com>
2025-01-27 18:00:38 +0100

    completed task 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text, repetitons) {
+    let newText ="";
+    if (repetitons >= 0){
+        for(let i = 0; i < repetitons; i++){
+            newText+=text;
+        }
+        return newText;
+    }else return "ERROR"
+    
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
