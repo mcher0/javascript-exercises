cc8208293330d6c05d5dab2a35d19e88dba6bc09
mcher0 <mansuperman.ms@gmail.com>
2025-01-28 18:01:13 +0100

    completed task 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitTemp) {
+    let rawCelsius = (farenheitTemp - 32) * (5/9);
+    let roundedCelcius = Number(rawCelsius.toFixed(1));
+    return roundedCelcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celciusTemp) {
+  let rawFarenheit = (celciusTemp*(9/5)+32);
+  let roundedFarenheit = Number(rawFarenheit.toFixed(1));
+  return roundedFarenheit;
 };
 
 // Do not edit below this line
