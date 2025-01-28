acd83ac33e0626f43835e737d4e262109c8c301b
mcher0 <mansuperman.ms@gmail.com>
2025-01-28 17:49:55 +0100

    completed task 4,5,6

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+    let elementPresent = true;
+    while (elementPresent){
+        for (arg of args){
+            console.log(arg)
+            if (array.indexOf(arg) !== -1){
+                array.splice(array.indexOf(arg),1);
+            }
+        }
+        if (array.indexOf(arg) == -1){
+            elementPresent = false;
+        }
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(lowerBound,upperBound) {
+    if ( lowerBound % 1 === 0 && upperBound % 1 === 0 && typeof lowerBound === "number" && typeof upperBound === "number" && lowerBound >= 0 && upperBound >= 0){
+        if (lowerBound > upperBound){
+            let temporaryStorage = upperBound;
+            upperBound = lowerBound;
+            lowerBound = temporaryStorage;
+        }  
+        for (let i = lowerBound+1; i <= upperBound; i++)
+        {
+            lowerBound+=i;
+        }
+        return lowerBound;
+    }else{
+        return "ERROR";
+    }
 
 };
 
